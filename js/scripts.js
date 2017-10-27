8b8c223085db3928993ea0e017242a6c7c0650f0
Michael Arrington <mike@Macrilicious-3.local>
2017-10-27 12:16:33 -0700

    Add JS click listening for navigation

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#nav-law").click(function(event) {
+    console.log("law");
+  });
+  $("#nav-lard").click(function(event) {
+    console.log("lard");
+  });
+  $("#nav-party").click(function(event) {
+    console.log("party");
+  });
+
+});
